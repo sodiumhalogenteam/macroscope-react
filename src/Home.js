f034c986f6ad1e273af6c7dbece7c5b451bcfc68
Adam 👾 <adamcurl6@gmail.com>
2019-07-11 15:28:16 -0500

    📦  NEW: adds date sorting and hidden file logic

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,12 @@ var s3 = new AWS.S3({
 });
 
 class Home extends Component {
-  state = { folders: [] };
+  state = {
+    folders: [],
+    hiddenFolders: [],
+    showHiddenFolders: false,
+    asc: false
+  };
 
   async componentDidMount() {
     const that = this;
@@ -22,8 +27,12 @@ class Home extends Component {
       function(err, data) {
         const len = data.Contents.length;
         let tempArr = [];
+        let hiddenTempArr = [];
         for (var i = 1; i < len; i++) {
-          if (data.Contents[i].Size === 0) {
+          if (
+            data.Contents[i].Size === 0 &&
+            !data.Contents[i].Key.includes("hide")
+          ) {
             const sdate = JSON.stringify(data.Contents[i].LastModified).replace(
               '"',
               ""
@@ -35,19 +44,70 @@ class Home extends Component {
               ).replace("/", ""),
               date: sdate.substring(0, sdate.indexOf("T"))
             });
+          } else if (data.Contents[i].Size === 0) {
+            const sdate = JSON.stringify(data.Contents[i].LastModified).replace(
+              '"',
+              ""
+            );
+            hiddenTempArr.push({
+              key: data.Contents[i].Key.replace(
+                `${that.props.match.params.project}/`,
+                ""
+              ).replace("/", ""),
+              date: sdate.substring(0, sdate.indexOf("T"))
+            });
           }
         }
         that.setState({ folders: tempArr });
+        that.setState({ hiddenFolders: hiddenTempArr });
       }
     );
   }
 
+  sortByDate() {
+    const { asc, folders } = this.state;
+    const tempArr = [...folders];
+
+    if (!asc) {
+      tempArr.sort(function(a, b) {
+        var keyA = new Date(a.date),
+          keyB = new Date(b.date);
+        // Compare the 2 dates
+        if (keyA < keyB) return -1;
+        if (keyA > keyB) return 1;
+        return 0;
+      });
+    } else {
+      tempArr.sort(function(a, b) {
+        var keyA = new Date(a.date),
+          keyB = new Date(b.date);
+        // Compare the 2 dates
+        if (keyA > keyB) return -1;
+        if (keyA < keyB) return 1;
+        return 0;
+      });
+    }
+
+    this.setState({ asc: !asc });
+    this.setState({ folders: tempArr });
+  }
+
+  toggleHidden() {
+    const { showHiddenFolders } = this.state;
+    this.setState({ showHiddenFolders: !showHiddenFolders });
+  }
+
   render() {
-    const { folders } = this.state;
+    const { folders, hiddenFolders, showHiddenFolders } = this.state;
+    console.log(folders);
+    console.log(hiddenFolders);
     return (
       <div className="grid">
         <div id="top-nav">
-          <Header currFolder="ata" project={this.props.match.params.project} />
+          <Header
+            currFolder={this.props.match.params.project}
+            project={this.props.match.params.project}
+          />
           <div className="center">
             <p>
               As we are working on your project, we will post files here for you
@@ -56,6 +116,17 @@ class Home extends Component {
               Choose a folder from below.
             </p>
             <ul className="folder_names">
+              <li className="list-title">FOLDER NAME</li>
+              <li>
+                <span>
+                  <button
+                    className="sort-btn"
+                    onClick={() => this.sortByDate()}
+                  >
+                    LAST UPDATED
+                  </button>
+                </span>
+              </li>
               {folders.length ? (
                 folders.map(folder => (
                   <div key={folder.key}>
@@ -81,24 +152,37 @@ class Home extends Component {
             </ul>
             <br />
 
-            {/* <a href="#" className="show_hide">
-          +
-        </a>
-        <div className="slidingDiv">
-          <ul className="folder_names hidden_folders">
-            <li>
-              <a href="archive_hide/index.html">archive </a>
-            </li>
-            <span>December 31 1969</span>
-            <li>
-              <a href="design_catalyst_hide/index.html">design catalyst </a>
-            </li>
-            <span>December 31 1969</span>
-          </ul>
-          <a href="#" className="show_hide">
-            +
-          </a>
-        </div> */}
+            <div className="slidingDiv">
+              {showHiddenFolders ? (
+                <ul className="folder_names hidden_folders">
+                  {hiddenFolders.length ? (
+                    hiddenFolders.map(folder => (
+                      <div key={folder.key}>
+                        <li>
+                          <Link
+                            to={`/${this.props.match.params.project}/${
+                              folder.key
+                            }/`}
+                          >
+                            {folder.key.replace(/-|_/g, " ")}
+                          </Link>
+                        </li>
+                        <li>
+                          <span>{folder.date}</span>
+                        </li>
+                      </div>
+                    ))
+                  ) : (
+                    <span className="white">
+                      Looks like there's nothing here...yet!
+                    </span>
+                  )}
+                </ul>
+              ) : null}
+              <button onClick={() => this.toggleHidden()} className="show_hide">
+                +
+              </button>
+            </div>
           </div>
         </div>
       </div>
